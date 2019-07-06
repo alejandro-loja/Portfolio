ccb189276680ca36077564d7c6f98191e17001fb
Alejandro Loja <alejandro.loja@gmail.com>
2019-07-06 16:32:34 -0400

    changed footer and reactrouterdom 440 -> 501

--- src/pages/Footer/index.jsx
+++ src/pages/Footer/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer className="bg-info p-4 right">
-            Copyright &copy;
+        <footer className="bg-info p-4 right footer" style={{marginTop:'200px'}}>
+            <p>2019 &copy; Alejandro L. All Rights Reserved.</p>
 </footer>
     );
 }
