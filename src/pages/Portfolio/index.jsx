9c1ad31684d46d3ee810ac68c0b0b2f3f2927afc
Alejandro Loja <alejandro.loja@gmail.com>
2019-07-06 22:24:56 -0400

    mobile responsive and about me portfolio page layout fixed.

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -5,7 +5,7 @@ import projects from "../../portfolioInfo.json";
 function Portfolio() {
   return (
     <div className="mb-4">
-      <div className=" offset-lg-1 col-lg-10 col-sm-12 shadow border bg-white">
+      <div className=" offset-lg-1 offset-xl-2 col-xl-8 col-lg-10 col-sm-12 shadow border bg-white">
         <h2 className="m-4">Portfolio</h2>
         <hr />
         <div className="row justify-content-around">
